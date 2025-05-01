09572a4e9bf2e263e5610dc94ad85b498fce1c01
Usmar A Padow <usmpadow@gmail.com>
2025-05-01 12:53:12 +0900

    added part of the save game functionality

--- mainGameScreen.lua
+++ mainGameScreen.lua
@@ -1911,6 +1911,36 @@ end
 
 
 function saveAffirmative()
+    --local serializableBoxes = getSerializableBoxes()
+    saveCaravan()
+    local data=composer.getVariable("caravan")
+    local jsonString = json.encode(data)
+    
+    local path = system.pathForFile("saveCaravanFile.json", system.DocumentsDirectory)--this is the sandbox directory
+    --local path = system.pathForFile("level.json", system.ResourceDirectory)--this is the main directory
+    local file, errorString = io.open(path, "w")
+    
+    if file then
+        file:write(jsonString)
+        io.close(file)
+        print("Level saved successfully.")
+    else
+        print("Error saving level: " .. errorString)
+    end
+    local data=composer.getVariable("characters")
+    local jsonString = json.encode(data)
+    
+    local path = system.pathForFile("saveChacartersFile.json", system.DocumentsDirectory)--this is the sandbox directory
+    --local path = system.pathForFile("level.json", system.ResourceDirectory)--this is the main directory
+    local file, errorString = io.open(path, "w")
+    
+    if file then
+        file:write(jsonString)
+        io.close(file)
+        print("Level saved successfully.")
+    else
+        print("Error saving level: " .. errorString)
+    end
 
 end
 
@@ -1933,7 +1963,34 @@ function saveProgress()
 end
 
 function loadAffirmative()
-
+    local path = system.pathForFile("saveCaravanFile.json", system.DocumentsDirectory)--this is the sandbox directory
+    local file, errorString = io.open(path, "r")
+    if file then
+        local jsonString = file:read("*a")
+        io.close(file)
+        local data = json.decode(jsonString)
+        composer.setVariable("caravan", data)
+        local obj={}
+        obj.rotation=composer.getVariable("caravan").rotation
+        obj.x=composer.getVariable("caravan").x
+        obj.y=composer.getVariable("caravan").y
+        setGamePausedState(true)
+        transition.to( caravanGroup, { time=1500, x=obj.x, y=obj.y, onComplete=transitionCompleted } )
+        updateCaravanCoordinates(obj)
+        caravan.rotation=obj.rotation
+        print("Teleported to box.x:" .. box.x .. ", box.y:" .. box.y)
+    else
+        pauseAndShowQuickMessageFast("error loading files")
+    end
+    local path = system.pathForFile("saveChacartersFile.json", system.DocumentsDirectory)--this is the sandbox directory
+    local file, errorString = io.open(path, "r")
+    if file then
+        local jsonString = file:read("*a")
+        io.close(file)
+        local data = json.decode(jsonString)
+        composer.setVariable("characters", data)
+    end
+    pauseAndShowQuickMessageFast("game loaded")
 end
 
 
@@ -1954,9 +2011,81 @@ function loadProgrress()
     unPauseGame
     )
 end
+function loadFromJson()
+    local path = system.pathForFile("level.json", system.ResourceDirectory)--this is the main directory
+    local file, errorString = io.open(path, "r")
+    if file then
+        local jsonString = file:read("*a")
+        io.close(file)
+        --print("json sting:"..jsonString)
+        local serializableBoxes = json.decode(jsonString)
+        
+        -- Clear existing boxes
+        for i = #tblBoxes, 1, -1 do
+            local box = tblBoxes[i]
+            box:removeSelf() -- Remove from display
+            table.remove(tblBoxes, i) -- Remove from table
+        end
+        --diagnostic
+        print("Number of items: " .. #serializableBoxes) -- Should match JSON array length
+        for i, data in ipairs(serializableBoxes) do
+            print("Item " .. i .. " type: " .. type(data)) -- Should be "table"
+            if type(data) == "table" then
+                print("Item " .. i .. " label: " .. tostring(data.label)) -- Should show label or "nil"
+            end
+            if data.label==nil then
+                print("nil label found in data, skipping")
+            else
+                -- Box creation code
+            end
+        end
+        local mistranlsEndInData=false
+        -- Recreate boxes from loaded data
+        for _, data in ipairs(serializableBoxes) do
+                if data.label==nil then
+                    print("nil label found in data, skipping")
+                else
+                --local box = display.newImageRect("img/block-white.png", data.size, data.size)
+                if data.color=="colorWhite" then
+                    box = display.newImageRect("img/block-white.png", 32, 32)    
+                elseif data.color=="colorRed" then
+                    box = display.newImageRect("img/block-red.png", 32, 32)    
+                elseif data.color=="colorGreen" then
+                    box = display.newImageRect("img/block-green.png", 32, 32)    
+                elseif data.color=="colorBlue" then
+                    box = display.newImageRect("img/block-blue.png", 32, 32)    
+                elseif data.color=="colorYellow" then
+                    box = display.newImageRect("img/block.png", 32, 32)    
+                end
+                print("label:"..data.label)
+                if data.label=="mist_end_exit" then
+                    mistranlsEndInData=true
+                end
+                box.label = data.label
+                box.color = data.color
+                box.x = data.x
+                box.y = data.y
+                box.size = data.size
+                box.alpha = 0
+                box:addEventListener("tap", boxListener) -- Add your tap listener if needed
+                table.insert(tblBoxes, box)
+            end
+        end
+        print("Level loaded successfully.")
+    else
+        print("Error loading level: " .. errorString)
+    end
+    if mistranlsEndInData then
+        print("mistrals end in data")
+    else
+        print("warning:mistrals end NOT in data")
+    end
+end
+
 -- Function to load level.json and reconstruct tblBoxes
 local function loadLevel()
-    loadProgrress()
+    
+    loadFromJson()
     --[[
     --local path = system.pathForFile("level.json", system.DocumentsDirectory)
     local path = system.pathForFile("level.json", system.ResourceDirectory)--this is the main directory
@@ -2030,80 +2159,10 @@ local function loadLevel()
     ]]
 end
 
-function loadLevel()
-    local path = system.pathForFile("level.json", system.ResourceDirectory)--this is the main directory
-    local file, errorString = io.open(path, "r")
-    if file then
-        local jsonString = file:read("*a")
-        io.close(file)
-        --print("json sting:"..jsonString)
-        local serializableBoxes = json.decode(jsonString)
-        
-        -- Clear existing boxes
-        for i = #tblBoxes, 1, -1 do
-            local box = tblBoxes[i]
-            box:removeSelf() -- Remove from display
-            table.remove(tblBoxes, i) -- Remove from table
-        end
-        --diagnostic
-        print("Number of items: " .. #serializableBoxes) -- Should match JSON array length
-        for i, data in ipairs(serializableBoxes) do
-            print("Item " .. i .. " type: " .. type(data)) -- Should be "table"
-            if type(data) == "table" then
-                print("Item " .. i .. " label: " .. tostring(data.label)) -- Should show label or "nil"
-            end
-            if data.label==nil then
-                print("nil label found in data, skipping")
-            else
-                -- Box creation code
-            end
-        end
-        local mistranlsEndInData=false
-        -- Recreate boxes from loaded data
-        for _, data in ipairs(serializableBoxes) do
-                if data.label==nil then
-                    print("nil label found in data, skipping")
-                else
-                --local box = display.newImageRect("img/block-white.png", data.size, data.size)
-                if data.color=="colorWhite" then
-                    box = display.newImageRect("img/block-white.png", 32, 32)    
-                elseif data.color=="colorRed" then
-                    box = display.newImageRect("img/block-red.png", 32, 32)    
-                elseif data.color=="colorGreen" then
-                    box = display.newImageRect("img/block-green.png", 32, 32)    
-                elseif data.color=="colorBlue" then
-                    box = display.newImageRect("img/block-blue.png", 32, 32)    
-                elseif data.color=="colorYellow" then
-                    box = display.newImageRect("img/block.png", 32, 32)    
-                end
-                print("label:"..data.label)
-                if data.label=="mist_end_exit" then
-                    mistranlsEndInData=true
-                end
-                box.label = data.label
-                box.color = data.color
-                box.x = data.x
-                box.y = data.y
-                box.size = data.size
-                box.alpha = 0
-                box:addEventListener("tap", boxListener) -- Add your tap listener if needed
-                table.insert(tblBoxes, box)
-            end
-        end
-        print("Level loaded successfully.")
-    else
-        print("Error loading level: " .. errorString)
-    end
-    if mistranlsEndInData then
-        print("mistrals end in data")
-    else
-        print("warning:mistrals end NOT in data")
-    end
-end
 
 -- Load button listener
 local function onLoadButtonTap(event)
-    loadLevel()
+    loadProgrress()
     return true
 end
 
